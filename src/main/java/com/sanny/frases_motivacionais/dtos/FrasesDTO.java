3957ddc5857ad2c2dfe813f675e8b733f9782917
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-24 19:22:47 -0300

    Criação de atributos.

---
+++ src/main/java/com/sanny/frases_motivacionais/dtos/FrasesDTO.java
@@ -0,0 +1,23 @@
+package com.sanny.frases_motivacionais.dtos;
+
+public class FrasesDTO {
+
+    private Long id;
+    private String frase;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrase() {
+        return frase;
+    }
+
+    public void setFrase(String frase) {
+        this.frase = frase;
+    }  
+}
