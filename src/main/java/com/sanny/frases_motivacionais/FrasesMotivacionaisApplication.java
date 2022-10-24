2a79394131aad5e514aa39b06332eecda96e65ee
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-24 16:06:58 -0300

    Primeiro commit

---
+++ src/main/java/com/sanny/frases_motivacionais/FrasesMotivacionaisApplication.java
@@ -0,0 +1,13 @@
+package com.sanny.frases_motivacionais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrasesMotivacionaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrasesMotivacionaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanny/frases_motivacionais/FrasesMotivacionaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanny.frases_motivacionais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrasesMotivacionaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
