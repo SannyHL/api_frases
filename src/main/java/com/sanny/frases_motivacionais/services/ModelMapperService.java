dee490d1d1c71c4b232ded74d4ab4a5574331a19
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-24 23:32:09 -0300

    Criação bean ModelMapper

---
+++ src/main/java/com/sanny/frases_motivacionais/services/ModelMapperService.java
@@ -0,0 +1,14 @@
+package com.sanny.frases_motivacionais.services;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ModelMapperService {
+    
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
