76db80333225acc86187ad033d9bc450de092fa8
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-24 23:36:25 -0300

    Criação de construtor para exception.

---
+++ src/main/java/com/sanny/frases_motivacionais/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.sanny.frases_motivacionais.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+    
+    public ObjectNotFoundException(String menssagem){
+        super(menssagem);
+    }
+
+}
