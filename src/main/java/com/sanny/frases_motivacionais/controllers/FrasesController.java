40c3baf7fb19585e20139487d150e4f68cf1ef09
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-26 16:28:10 -0300

    Adição de create.

--- src/main/java/com/sanny/frases_motivacionais/controllers/FrasesController.java
+++ src/main/java/com/sanny/frases_motivacionais/controllers/FrasesController.java
@@ -1,5 +1,6 @@
 package com.sanny.frases_motivacionais.controllers;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,10 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sanny.frases_motivacionais.dtos.FrasesDTO;
+import com.sanny.frases_motivacionais.models.FrasesModel;
 import com.sanny.frases_motivacionais.services.FrasesService;
 
 @RestController
@@ -30,7 +35,14 @@ public class FrasesController {
     }
 
     @GetMapping(ID)
-    public ResponseEntity<FrasesDTO> findById(@PathVariable Long id){
+    public ResponseEntity<FrasesDTO> findById(@PathVariable Long id) throws Exception{
         return ResponseEntity.ok().body(mapper.map(service.findById(id), FrasesDTO.class));
+    }
+
+    @PostMapping
+    public ResponseEntity<FrasesDTO> create(@RequestBody FrasesDTO frasesDTO){
+        FrasesModel frasesModel = service.create(frasesDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path(ID).buildAndExpand(frasesModel.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
